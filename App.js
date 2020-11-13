42de5d9201144025ab8c96052ead7222c2d8e1b3
z3t4z00k <z3t4z00k@gmail.com>
2020-11-13 06:02:05 -0500

    Imported useSTate

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import * as Font from 'expo-font';
