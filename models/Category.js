58ab2def92117bfe7a1f41435447cd93ec84be3e
z3t4z00k <z3t4z00k@gmail.com>
2020-11-13 14:34:47 -0500

    Created a model for category

---
+++ models/Category.js
@@ -0,0 +1,10 @@
+class Category{
+	constructor(id, title, bgColor, fgColor){
+		this.id = id;
+		this.title = title;
+		this.bgColor = bgColor;
+		this.fgColor = fgColor;
+	}
+}
+
+export default Category;
