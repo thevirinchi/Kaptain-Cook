53c9af0d8f69e4a6ac8b6c0a4d726b66492a06f2
z3t4z00k <z3t4z00k@gmail.com>
2020-11-13 14:35:59 -0500

    Changing screen headers in accordance with the category selected

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import {View, Text, Button, StyleSheet, ColorPropType} from 'react-native'
 
+import {Categories} from '../data/data'
+
 const CategoryMealsScreen = props => {
+
 	return (
 		<View style={styles.container}>
 			<Button title="Go to Meal!" onPress={()=>{props.navigation.push("MealDetails")}}/>
 			<Button title="Go back" onPress={()=>{props.navigation.goBack()}}/>
 		</View>
 	)
+}
+
+CategoryMealsScreen.navigationOptions = navigationData => {
+	const cat = Categories.find(cat => cat.id === navigationData.navigation.getParam('categoryId'))
+	return{
+		headerTitle: cat.title,
+		headerStyle: {
+			backgroundColor: cat.bgColor
+		},
+		headerTintColor: cat.fgColor
+	}
 }
 
 const styles = StyleSheet.create({
