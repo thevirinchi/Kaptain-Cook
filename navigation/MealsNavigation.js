2ae93da3075170282accfdd3034e167d1dec1598
z3t4z00k <z3t4z00k@gmail.com>
2020-11-16 04:30:27 -0500

    Added bottom nav bar

--- navigation/MealsNavigation.js
+++ navigation/MealsNavigation.js
@@ -1,9 +1,16 @@
+import React from 'react'
+
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 import { createAppContainer } from 'react-navigation'
 
 import CategoriesScreen from "../screens/CategoriesScreen"
 import CategoryMealsScreen from "../screens/CategoryMealsScreen"
+import FavoritesScreen from '../screens/FavoritesScreen'
 import MealDetailsScreen from "../screens/MealDetailsScreen"
+
+import {Ionicons} from '@expo/vector-icons'
+import Colors from '../constants/Colors/light'
 
 const MealsNavigator = createStackNavigator({
 	Categories: CategoriesScreen,
@@ -11,4 +18,28 @@ const MealsNavigator = createStackNavigator({
 	MealDetails: MealDetailsScreen
 })
 
-export default createAppContainer(MealsNavigator)
+const MealFavTabNavigator = createBottomTabNavigator({
+	Meals: {
+		screen: MealsNavigator,
+		navigationOptions: {
+			tabBarIcon: (tabInfo) => {
+				return <Ionicons name="ios-restaurant" size={24} color={tabInfo.tintColor}/>
+			}
+		}
+	},
+	Favorites: {
+		screen: FavoritesScreen,
+		navigationOptions: {
+			tabBarIcon: (tabInfo) => {
+				return <Ionicons name="ios-star" size={24} color={tabInfo.tintColor}/>
+			}
+		}
+	}
+},
+{
+	tabBarOptions: {
+		activeTintColor: Colors.primary,
+	}
+})
+
+export default createAppContainer(MealFavTabNavigator)
