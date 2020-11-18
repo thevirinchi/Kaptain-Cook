72c890c0485dfe42d1f091210cff681c6780ac72
z3t4z00k <z3t4z00k@gmail.com>
2020-11-18 08:07:39 -0500

    Added list of available filters

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,17 +1,31 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Switch } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 
 import RightButton from '../components/Header/RightButton'
+import Primary from '../components/Typo/Heading/Primary'
+import Body from '../components/Typo/Body/Body'
 
-import { Categories, MEALS } from '../data/data'
+import { Filters } from '../data/data'
 
 import Colors from "../constants/Colors/light"
+import { FlatList } from 'react-native-gesture-handler'
 
 const FiltersScreen = props => {
+
+	const renderFilterItem = itemData => {
+		return (
+			<View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center", paddingVertical: 4, paddingHorizontal: 4}}>
+				<Body text={itemData.item}/>
+				<Switch/>
+			</View>
+		)
+	}
+
 	return (
 		<View style={styles.container}>
-			<Text>Filters Screen</Text>
+			<Primary text="Available Filters"/>
+			<FlatList numColumns={1} data={Filters} renderItem={renderFilterItem} backgroundColor={Colors.whiteLight} width={"100%"} style={{marginTop: 16}}/>
 		</View>
 	)
 }
@@ -30,9 +44,11 @@ FiltersScreen.navigationOptions = navData => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		flexDirection: "column",
+		backgroundColor: Colors.whiteLight,
 		justifyContent: "center",
-		alignItems: "center"
+		alignItems: "center",
+		paddingVertical: 16,
+		paddingHorizontal: 24
 	}
 })
 
