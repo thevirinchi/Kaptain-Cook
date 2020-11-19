14316e97d33a951ef2ba3f4a2ac120e133901cd0
z3t4z00k <z3t4z00k@gmail.com>
2020-11-19 10:39:14 -0500

    Added action to set filters

--- state/meals/actions.js
+++ state/meals/actions.js
@@ -1,8 +1,13 @@
 export const TOGGLE_FAV = 'TOGGLE_FAV'
+export const SET_FILTERS = 'SET_FILTERS'
 
 export const toggleFav = (id) => {
 	return {
 		type: TOGGLE_FAV,
 		mealId: id
 	}
+}
+
+export const setFilters = filterSettings => {
+	return {type: SET_FILTERS, filters: filterSettings}
 }
